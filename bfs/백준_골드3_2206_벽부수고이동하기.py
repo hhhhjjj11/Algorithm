c34c9132f284a087727e713fad976c4c0ea0fbec
허재 <huhwo00@gmail.com>
2023-09-22 18:03:57 +0900

    docs: 230922

--- bfs/백준_골드3_2206_벽부수고이동하기.py
+++ bfs/백준_골드3_2206_벽부수고이동하기.py
@@ -1,28 +1,31 @@
 # 백준 / 골드3 / 2206 / 벽부수고 이동하기
 
-
-# 풀이
+# 풀이 
 import sys
 from collections import deque
 
-N, M = map(int,input().split())
+N,M = map(int,sys.stdin.readline())
 
-graph = []
-check = []
-for i in range(N):
-    graph.append(list(map(int,sys.stdin.readline().split())))
-    check.append([0]*M)
+graph = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
+
+visited = [[[0,0]* M] for _ in range(N)]
 
 Q = deque([])
-Q.append([0,0])
+Q.append([0,0,0])
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
 while Q:
-    Cx, Cy = Q.popleft()
+    
+    Cx, Cy, isbroke = Q.popleft()
+    
     for i in range(4):
         Nx = Cx + dx[i]
         Ny = Cy + dy[i]
 
-        if 0<=Nx<M and 0<=Ny<N and graph[Nx][Ny]==0 and :
+        if 0<=Nx<N and 0<=Nx<M:
+            if isbroke == 0:  # 만약에 벽을 뚫은 적이 없는 상태라면
+                pass
+                if visited[Nx][Ny][0]
+            else: # 벽을 뚫은 경우
