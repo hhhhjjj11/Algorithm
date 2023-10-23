b41782774536c695607484388dff63988645bb7e
허재 <huhwo00@gmail.com>
2023-10-23 23:19:29 +0900

    231023

--- 누적합/백준_골5_3020_개똥벌레.py
+++ 누적합/백준_골5_3020_개똥벌레.py
@@ -1,46 +1,40 @@
 # 정리
 """
+구간에 해당하는 수를 세는거를 누적합 아이디어를 적용해서 쉽게 계산한다는거...
+골드5이지만 나는 생각도 못했다;;
 
 """
 # 풀이
 import sys
 
-N, H = map(int,sys.stdin.readline().strip().split())
+N, H = map(int,sys.stdin.readline().strip().split())    # 0,1,2,3,4,...,H-1
 
-dp = [0]*(H+1)
-dp2 = [0]*(H+1)
+up = [0]*H
+down = [0]*H
 
 for i in range(N):
     if i%2 == 0:
         # 석순
         h = int(input())
-        dp[h] += 1
+        down[h-1] += 1
     else:
         # 종유석
         h = int(input())
-        dp2[h] += 1
+        up[h-1] += 1
 
-s = [0]*(H+1)
-s[0] = 9999999
-print('dp', dp)
-print('dp2', dp2)
 
-for i in range(1,H+1):
-    if dp[i]:
-        for x in range(dp[i]):
-            for k in range(1, i+1):
-                s[k] += 1
-    if dp2[i]:
-        for x in range(dp2[i]):
-            for l in range(H, H-i,-1):
-                s[l] += 1
+for i in range(H-2, -1,-1):
+    down[i] += down[i+1]
+    up[i] += up[i+1]
 
-print('s', s)
+min = 99999999
+count = 0
+for i in range(H):
+    # 장애물 세기
+    if min > up[i] + down[H-1-i]:
+        min = up[i] + down[H-1-i]
+        count = 1
+    elif min == up[i] + down[H-1-i]:
+        count += 1
 
-min = min(s)
-cnt =0
-for i in range(H+1):
-    if min == s[i]:
-        cnt += 1
-
-print(min, cnt)
+print(min, count)
