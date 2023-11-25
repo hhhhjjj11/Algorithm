84ffbd6afe99fb6c3379003d32e8e7f3ca4a9c75
허재 <huhwo00@gmail.com>
2023-11-25 18:54:01 +0900

    231125

--- PracticeNote.py
+++ PracticeNote.py
@@ -1,41 +1,69 @@
-"""
-바텀업으로 풀면 안되는이유 
-바텀업으로 푸는게 적합한 경우
+def solution(friends, gifts):
+ 
+    N = len(friends)
+    record =[[0]*N for _ in range(N)]
+    results = [0]*N
 
-값으로 바로 하는것보다는 해당 칸에서 코너횟수랑 직선거리횟수로 생각하는게 나음.
-"""
-import sys
-sys.setrecursionlimit(10**6)
+    # 입력데이터 gifts를 통해서 record를 완성 
+    # record[i][j] = i번 친구가 j번 친구에게 준 선물 갯수
+    for gift in gifts:
+        A,B = gift.split()
+        friends_idx_of_A = friends.index(A)
+        friends_idx_of_B = friends.index(B)
+        # print(friends_idx_of_A,friends_idx_of_B)
+        record[friends_idx_of_A][friends_idx_of_B]+=1
+        # print(A,B)
 
-di = [0,0,1,-1]
-dj = [1,-1,0,0]
+    # print(record)
+    temp = [0]*N
+    # 이제, 선물 예측.
+    for i in range(N):
+        for j in range(i+1,N):
+            if i==j:
+                continue
+            # print(i,j,'비교')
+            # print(friends[i],friends[j],'비교')
+            # 두 사람 사이에 주고받은 선물이 차이가 있으면
+            ItoJ, JtoI = record[i][j], record[j][i]
+            if ItoJ > JtoI: # I가 준게 더 많으면
+                # print('i가준게 더 많음')
+                # print(friends[i], '+1')
+                results[i]+=1
+                # print('1=====', results)
+            elif ItoJ < JtoI:
+                # print('j가준게 더 많음')
+                # print(friends[j],' +1')
+                results[j]+=1
+                # print('2=====', results)
 
-def solution(board):
-    N = len(board)
-    dp = [[9999999]*N for _ in range(N)]
-    
-    def dfs(i,j,d_now,V): # dfs(i,j) = i,j 부터 도착지점까지 걸리는 비용
-        # print('i,j',i,j, '/', V)
-        if dp[i][j] > V:
-            dp[i][j] = V
+            else: # 주고 받은 갯수가 같으면
+                # 선물지수를 따져야함.
+                # 선물지수 = 준 선물 갯수 - 받은 선물 갯수
+                present_value_of_i, present_value_of_j = 0,0
+                # 준거 더하기
+                present_value_of_i += sum(record[i])
+                present_value_of_j += sum(record[j])
+                # 받은거 빼기
+                for p in range(N):
+                    present_value_of_i -= record[p][i]
+                    present_value_of_j -= record[p][j]
 
-        for d in range(4):
-            i_next, j_next = i + di[d] , j + dj[d]
-            if 0<=i_next<N and 0<=j_next<N and board[i_next][j_next] != 1:
-                # 방향이 같은 경우랑 다른경우 분기
-                if d_now == d and V+100 < dp[i_next][j_next]+600:  # 방향이 같은경우
-                    dfs(i_next,j_next,d, V+100)
-                    if i==1 and j==1 :
-                        print('1', d_now, d, V, V+100)
-                elif d_now != d and V+600 <= dp[i_next][j_next]+600: # 방향을 꺾는경우  # 값이 같아도 탐색 해야함
-                    dfs(i_next,j_next,d, V+600)
-                    if i==1 and j==1 :
-                        print('2', d_now, d, V, V+600)
+                if present_value_of_i > present_value_of_j:
+                    results[i] += 1
+                    # print(friends[i], '+1')
+                    # print('3=====', results)
+                elif present_value_of_i < present_value_of_j:
+                    results[j] += 1
+                    # print(friends[j],' +1')
+                    # print('4=====', results)
+                temp[i]= present_value_of_i
+                temp[j] = present_value_of_j
+                # 같으면 주고받지 않음.
+    # print(temp)
+    # print(results)
+    return max(results)
 
-    dfs(0,0,0,0)
-    dfs(0,0,2,0)
-    # for row in dp:
-    #     print(row)
-    return dp[N-1][N-1]
 
-print(solution([[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]]))
+
+print(solution(
+["joy", "brad", "alessandro", "conan", "david"], ["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"]))
--- dfs/18. 프로그래머스_LV3_경주로건설.py
+++ dfs/18. 프로그래머스_LV3_경주로건설.py
@@ -1,8 +1,18 @@
 # 정리
 """
+왜 bottom up으로 하면 안되는 거지? 
+1. botom up 으로 풀면 -> dfs(i,j) = i,j에서 도착지점까지 최소 비용
++ dp에 값이 있으면 탐색하지 않고 그 값을 그냥 리턴함. (그렇지 않으면..? 그렇지않으면 ....음... )
+다음 칸에서 계산한 값이 정답케이스의 일부가 된다.
+
+2. 그런데 이거는 틀렸음 왜냐하면
+
+
+bottom up 으로 풀었던 문제 경로의 갯수
+
+bottom up 문제가 회전을 포함 못시키는것같은데 바텀업으로도 될것같은데??
 
 """
-
 # 풀이
 import sys
 sys.setrecursionlimit(10**6)
@@ -11,29 +21,84 @@ di = [0,0,1,-1]
 dj = [1,-1,0,0]
 
 def solution(board):
+    print('sdfsdfsdfsdf')
     N = len(board)
-    dp = [[9999999]*N for _ in range(N)]
-    
-    def dfs(i,j,d_now,V): # dfs(i,j) = i,j 부터 도착지점까지 걸리는 비용
-        # print('i,j',i,j, '/', V)
-        if dp[i][j] > V:
-            dp[i][j] = V
+    dp = [[[9999999,-1]]*N for _ in range(N)]
+    visited = [[0]*N for _ in range(N)]
 
+    print('sdfsdfsdfsdf')
+    def dfs(i,j,d_now): # dfs(i,j) = i,j 부터 도착지점까지 걸리는 비용
+        print(i,j)
+        if i==N-1 and j==N-1:
+            return 0
+        
+        if not (i==0 and j==0) and visited[i][j]:
+            print('반환',)
+            return dp[i][j][0]
+        
         for d in range(4):
             i_next, j_next = i + di[d] , j + dj[d]
-            if 0<=i_next<N and 0<=j_next<N and board[i_next][j_next] != 1:
+            # 가본적이 없는 곳이면
+            if 0<=i_next<N and 0<=j_next<N and board[i_next][j_next] != 1 and not visited[i_next][j_next]:
                 # 방향이 같은 경우랑 다른경우 분기
-                if d_now == d and V+100 < dp[i_next][j_next]+600:  # 방향이 같은경우
-                    dfs(i_next,j_next,d, V+100)
-                    if i==1 and j==1 :
-                        print('1', d_now, d, V, V+100)
-                elif d_now != d and V+600 <= dp[i_next][j_next]: # 방향을 꺾는경우  # 값이 같아도 탐색 해야함
-                    dfs(i_next,j_next,d, V+600)
-                    if i==1 and j==1 :
-                        print('2', d_now, d, V, V+600)
+                visited[i_next][j_next] =1
+                TEMP = dfs(i_next,j_next,d)
+                visited[i_next][j_next] = 0
+                if d_now == d and TEMP+100 :  # 방향이 같은경우
+                    
+                    print('i,j', i,j, '/', 'i_next, j_next', i_next, j_next)
+                    dp[i][j] = [TEMP+100, d]
+                elif d_now != d: # 방향을 꺾는경우  # 값이 같아도 탐색 해야함
+                    print('i,j', i,j, '/', 'i_next, j_next', i_next, j_next)
+                    dp[i][j] = [TEMP+600, d]
+            elif 0<=i_next<N and 0<=j_next<N and board[i_next][j_next] != 1 and visited[i_next][j_next]: #가본적이 있는곳
+                # 만약에 방향이 다르면 + 500을 더함. 그리고 그 값이 현재 dp 값 보다 작으면 갱신.
+                if dp[i_next][j_next][1] != d_now:
+                    if dp[i_next][j_next][0] + 600 < dp[i][j][0]:
+                        print('i,j', i,j, '/', 'i_next, j_next', i_next, j_next)
+                        dp[i][j] = [dp[i_next][j_next][0] + 600, d_now]
+                # 만약에 방향이 같으면 100을 더해보고. 그리고 그 값이 현재 dp 값보다 작으면 갱신.
+                else:
+                    if dp[i_next][j_next][0] + 100 < dp[i][j][0]:
+                        print('i,j', i,j, '/', 'i_next, j_next', i_next, j_next)
+                        dp[i][j] = [dp[i_next][j_next][0] + 100, d_now]
 
-    dfs(0,0,0,0)
-    dfs(0,0,2,0)
-    # for row in dp:
-    #     print(row)
-    return dp[N-1][N-1]
+        return dp[i][j][0]
+    print('sdfsdfsdfsdf')
+    dp[0][0][1] = 0
+    dfs(0,0,0)
+    dp[0][0][1] = 2
+    dfs(0,0,2)
+    print('sdfsdfsdfsdf')
+    return dp[0][0][0]
+
+print(solution([[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]]))
+
+# # 풀이
+# import sys
+# sys.setrecursionlimit(10**6)
+
+# di = [0,0,1,-1]
+# dj = [1,-1,0,0]
+
+# def solution(board):
+#     N = len(board)
+#     dp = [[9999999]*N for _ in range(N)]
+    
+#     def dfs(i,j,d_now,V): # dfs(i,j) = i,j 부터 도착지점까지 걸리는 비용
+#         if dp[i][j] > V:
+#             dp[i][j] = V
+
+#         for d in range(4):
+#             i_next, j_next = i + di[d] , j + dj[d]
+#             if 0<=i_next<N and 0<=j_next<N and board[i_next][j_next] != 1:
+#                 # 방향이 같은 경우랑 다른경우 분기
+#                 if d_now == d and V+100 < dp[i_next][j_next]+600:  # 방향이 같은경우
+#                     dfs(i_next,j_next,d, V+100)
+#                 elif d_now != d and V+600 <= dp[i_next][j_next]: # 방향을 꺾는경우  # 값이 같아도 탐색 해야함
+#                     dfs(i_next,j_next,d, V+600)
+
+#     dfs(0,0,0,0)
+#     dfs(0,0,2,0)
+
+#     return dp[N-1][N-1]
